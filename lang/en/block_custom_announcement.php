414d16c6b42ccb603d941e0f2d088857ddaa23f7
DhaaraniDevi <110713837+DhaaraniDevi@users.noreply.github.com>
2025-07-23 22:19:08 +0530

    Create block_custom_announcement.php

---
+++ lang/en/block_custom_announcement.php
@@ -0,0 +1,12 @@
+<?php
+$string['pluginname'] = 'Custom Announcement';
+$string['announcementtitle'] = 'Announcement Title';
+$string['announcementtext'] = 'Announcement Text';
+$string['backgroundcolor'] = 'Background Color';
+$string['backgroundcolor_help'] = 'Hex color code for the Background Color of the announcement.';
+$string['textcolor'] = 'Text Color';
+$string['textcolor_help'] = 'Hex color code for the Text Color of the announcement.';
+$string['defaulttitle'] = 'Site Announcement';
+$string['defaulttext'] = 'No announcements have been set yet.';
+$string['custom_announcement:addinstance'] = 'Add a new Custom Announcement block';
+$string['custom_announcement:myaddinstance'] = 'Add a new Custom Announcement block to Dashboard';
